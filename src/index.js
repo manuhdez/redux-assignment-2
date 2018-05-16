c0e099139956ad8db10a32eb2162fd86640cc7ab
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-05-16 22:08:49 +0100

    connected redux to react

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
+import reducer from './store/reducer';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const reducer = {
-  persons: []
-}
-
+//store
 const store = createStore(reducer);
 
-ReactDOM.render(<Provider/><App /></Provider>, document.getElementById('root'));
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  persons: [
+    {name: 'Manu', age: 27}
+  ]
+}
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  return state;
+}
+
+export default reducer;
