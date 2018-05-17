5a1e4cd45c46c61cfc3d5bee2d282347564a47c0
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-05-17 18:53:25 +0100

    connected actions to the component and made it work

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -5,29 +5,8 @@ import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
 class Persons extends Component {
-    // state = {
-    //     persons: []
-    // }
-
-    // personAddedHandler = () => {
-    //     const newPerson = {
-    //         id: Math.random(), // not really unique but good enough here!
-    //         name: 'Max',
-    //         age: Math.floor( Math.random() * 40 )
-    //     }
-    //     this.setState( ( prevState ) => {
-    //         return { persons: prevState.persons.concat(newPerson)}
-    //     } );
-    // }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
 
     render () {
-        // console.log(this.props.prs);
         return (
             <div>
                 <AddPerson personAdded={this.props.personAddedHandler} />
@@ -36,7 +15,7 @@ class Persons extends Component {
                         key={person.id}
                         name={person.name}
                         age={person.age}
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        clicked={() => this.props.personDeletedHandler(person.id)}/>
                 ))}
             </div>
         );
@@ -51,7 +30,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        personAddedHandler: () => dispatch({type: 'ADD_PERSON'})
+        personAddedHandler: () => dispatch({type: 'ADD_PERSON'}),
+        personDeletedHandler: (personId) => dispatch({type: 'REMOVE_PERSON', personId: personId})
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,19 +1,29 @@
+import * as actionTypes from './actions';
+
 const initialState = {
   persons: []
 }
 
 // Reducer
 const reducer = (state = initialState, action) => {
-  if (action.type === 'ADD_PERSON') {
+  switch (action.type) {
+    case actionTypes.ADD_PERSON:
       const newPerson = {
-          id: Math.random(), // not really unique but good enough here!
-          name: 'Max',
-          age: Math.floor( Math.random() * 40 )
+        id: Math.random(), // not really unique but good enough here!
+        name: 'Max',
+        age: Math.floor( Math.random() * 40 )
       }
-      const newState = {...state};
-      newState.persons.push(newPerson);
 
-      return newState;
+      return {
+        ...state,
+        persons: state.persons.concat(newPerson)
+      }
+
+    case actionTypes.REMOVE_PERSON:
+      return {
+        ...state,
+        persons: state.persons.filter(person => person.id !== action.personId)
+      }
   }
 
   return state;
